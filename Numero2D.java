c23cc2c42420e7c028afe2f5e25578c051976b0d
Nunzio Strada <44925480+Stranu@users.noreply.github.com>
2018-11-20 14:55:46 +0100

    Update Numero2D.java

--- Numero2D.java
+++ Numero2D.java
@@ -9,9 +9,8 @@ public class Numero2D extends NumeroComposto {
 	}
 	
 	public Double distanza(Numero2D x2) {
+		//restituisce la distanza dei due punti su di un piano cartesiano
 		return  Math.sqrt(Math.pow(this.getX()-x2.getX(), 2)+Math.pow(this.getY()-x2.getY(), 2));
 	}
-	
-	
 
 }
